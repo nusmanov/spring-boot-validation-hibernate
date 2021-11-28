dc32518f2d7d6f51c169c9ecd9405facd0a7415d
nusmanov <nodirbek.usmanov@sevdesk.de>
2021-11-28 17:59:05 +0100

    validation

---
+++ src/main/kotlin/com/example/bookvalidation/BookValidationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bookvalidation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookValidationApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookValidationApplication>(*args)
+}
